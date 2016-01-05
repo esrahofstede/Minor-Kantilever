c00daae002bcc5b25850697d440735baa74adec0
Esra Hofstede <EsraH@minor.infosupport.edu>
2016-01-05 10:08:28 +0100

    string name key added, dummy operation created

---
+++ 06-PcSBestellen/Case3.PcSBestellen.Contract/IPcSBestellenService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Case3.PcSBestellen.Contract
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IPcSBestellenService
+    {
+        [OperationContract]
+        string SayHelloTest(string name);
+    }
+   
+}
---
+++ 06-PcSBestellen/Case3.PcSBestellen.Implementation/PcSBestellenServiceHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Case3.PcSBestellen.Contract;
+
+namespace Case3.PcSBestellen.Implementation
+{
+    public class PcSBestellenServiceHandler : IPcSBestellenService
+    {
+        public string SayHelloTest(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
