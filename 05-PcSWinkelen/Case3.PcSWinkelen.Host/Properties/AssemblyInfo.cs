0dfd8ec4dada45710ba01a87c0befc9e07464079
Esra Hofstede <EsraH@minor.infosupport.edu>
2016-01-05 09:41:43 +0100

    change project type to wcf libraries

--- 05-PcSWinkelen/Case3.PcSWinkelen.Host/Properties/AssemblyInfo.cs
+++ 05-PcSWinkelen/Case3.PcSWinkelen.Host/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ea349a65-367a-44d9-bf96-b91e98ee1745")]
+[assembly: Guid("53c3c7b8-4e84-4a7f-bdd5-471b3b946289")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
