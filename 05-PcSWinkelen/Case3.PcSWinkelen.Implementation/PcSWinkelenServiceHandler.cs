8387fd86bac92a7d4b2a271a069547ec8992c1bf
Esra Hofstede <EsraH@minor.infosupport.edu>
2016-01-06 15:36:10 +0100

    PcSWinkelen en bestellen log4net aangepast, servicebus toegevoegd

--- 05-PcSWinkelen/Case3.PcSWinkelen.Implementation/PcSWinkelenServiceHandler.cs
+++ 05-PcSWinkelen/Case3.PcSWinkelen.Implementation/PcSWinkelenServiceHandler.cs
@@ -4,17 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Case3.PcSWinkelen.Contract;
+using log4net;
 
 namespace Case3.PcSWinkelen.Implementation
 {
     public class PcSWinkelenServiceHandler : IPcSWinkelenService
     {
+        private static ILog _logger = LogManager.GetLogger(typeof(PcSWinkelenServiceHandler));
+
+        public PcSWinkelenServiceHandler()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
         public string SayHelloTest(string name)
         {
+            string greeting = "";
 
-            
-
-            return "Hello" + name + "! This is a test method.";
+            try
+            {
+                greeting = "Hello" + name + "! This is a test method.";
+            }
+            catch (Exception ex)
+            {
+                _logger.Fatal(ex.Message);
+            }
+            return greeting;
         }
     }
 }
--- 06-PcSBestellen/Case3.PcSBestellen.Implementation/PcSBestellenServiceHandler.cs
+++ 06-PcSBestellen/Case3.PcSBestellen.Implementation/PcSBestellenServiceHandler.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Case3.PcSBestellen.Contract;
+using log4net;
 
 namespace Case3.PcSBestellen.Implementation
 {
     public class PcSBestellenServiceHandler : IPcSBestellenService
     {
+        private static ILog _logger = LogManager.GetLogger(typeof(PcSBestellenServiceHandler));
+
+        public PcSBestellenServiceHandler()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
         public string SayHelloTest(string name)
         {
-            return "Hello" + name + "! This is a test method.";
+            string greeting = "";
+
+            try
+            {
+                greeting = "Hello" + name + "! This is a test method.";
+            }
+            catch (Exception ex)
+            {
+                _logger.Fatal(ex.Message);
+            }
+            return greeting;
         }
     }
 }
