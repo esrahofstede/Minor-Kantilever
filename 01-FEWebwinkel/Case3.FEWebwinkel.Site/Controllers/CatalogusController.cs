124c2525aeec9a319e91b511a93f6bb6da973ed5
unknown <BartvH@CURSISTZ04.minor.infosupport.edu>
2016-01-06 11:37:17 +0100

    Added temp ViewModel for Catalogus

---
+++ 01-FEWebwinkel/Case3.FEWebwinkel.Site/Controllers/CatalogusController.cs
@@ -0,0 +1,24 @@
+﻿using Case3.FEWebwinkel.Site.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Case3.FEWebwinkel.Site.Controllers
+{
+    public class CatalogusController : Controller
+    {
+        // GET: Catalogus
+        public ActionResult Index()
+        {
+
+            //var a = new CatalogusViewModel() { Naam = "test", Afbeelding="" } 
+
+            //List<CatalogusViewModel> lijst = new List<CatalogusViewModel>();
+
+
+            return View();
+        }
+    }
+}
