d38e56fddb1a63554898b52701ee85a074284914
Dennis Meijer <dnns11@hotmail.com>
2016-01-06 11:13:02 +0100

    Started new fearure

--- 01-FEWebwinkel/Case3.FEWebwinkel.Site/Controllers/HomeController.cs
+++ 01-FEWebwinkel/Case3.FEWebwinkel.Site/Controllers/HomeController.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Minor.ServiceBus.Agent.Implementation;
 using System.Web.Mvc;
 
 namespace Case3.FEWebwinkel.Site.Controllers
 {
     public class HomeController : Controller
     {
+        //private ServiceFactory<ICatalogusBeheer> _factory;
+
         public ActionResult Index()
         {
+            
+            /*ICatalogusBeheer agent = _factory.CreateAgent();
+            MsgFindProductsResult result = agent.FindProducts(new MsgFindProductsRequest());
+
+            ViewBag.Products = result.Products;*/
+
+
+
+            /*ICatalogusBeheer catalogusBeheerClientProxy = new CatalogusBeheerClient("", "http://minorbldsrv:4000/Design_Time_Addresses/Kantilever/BSCatalogusBeheer/");
+            MsgFindProductsRequest findProductRequestMessage = new MsgFindProductsRequest();
+            MsgFindProductsResult findProductResultMessage = catalogusBeheerClientProxy.FindProducts(findProductRequestMessage);
+            */
+
+
             return View();
         }
 
