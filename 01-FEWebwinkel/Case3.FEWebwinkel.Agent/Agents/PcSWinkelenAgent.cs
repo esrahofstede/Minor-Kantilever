d9c751868f12ee99d6245647a208dedd3b84aa5e
Rogier Welten <rogierwelten@gmail.com>
2016-01-07 12:47:43 +0100

    Koppeling in Frontend gemaakt op basis van de xsd's en wsdl's

---
+++ 01-FEWebwinkel/Case3.FEWebwinkel.Agent/Agents/PcSWinkelenAgent.cs
@@ -0,0 +1,42 @@
+﻿using Case3.FEWebwinkel.Agent.Interfaces;
+using Case3.BSCatalogusBeheer.Schema.Product;
+using Case3.BSCatalogusBeheer.Schema.Categorie;
+using Case3.PcSWinkelen;
+using Case3.PcSWinkelen.Messages;
+using Case3.PcSWinkelen.Schema;
+using Minor.ServiceBus.Agent.Implementation;
+using System.Collections.Generic;
+using System;
+
+namespace Case3.FEWebwinkel.Agent
+{
+    public class PcSWinkelenAgent : IPcSWinkelenAgent
+    {
+        private ServiceFactory<IPcSWinkelenService> _factory;
+        private IPcSWinkelenService _agent;
+
+        public PcSWinkelenAgent()
+        {
+            _factory = new ServiceFactory<IPcSWinkelenService>("PcSWinkelen");
+            _agent = _factory.CreateAgent();
+        }
+
+        public PcSWinkelenAgent(IPcSWinkelenService agent)
+        {
+            _agent = agent;
+        }
+
+        public CatalogusCollection GetProducts(int page, int pageSize)
+        {
+            
+            FindCatalogusResponseMessage result = _agent.GetCatalogusItems(new FindCatalogusRequestMessage() { Page = page, PageSize = pageSize });
+            return result.Products;
+        }
+
+        public CatalogusCollection GetProducts()
+        {
+            FindCatalogusResponseMessage result = _agent.GetCatalogusItems(new FindCatalogusRequestMessage() { Page = 1, PageSize = 20 });
+            return result.Products;
+        }
+    }
+}
