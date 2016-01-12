2b84b207b721d761707fb735d662d27e3eff1aa9
Boaz Cornellissen <BoazC@minor.infosupport.edu>
2016-01-12 15:34:03 +0100

    Added controller and view for inpakscherm

---
+++ 02-FEBestellingen/Case3.FEBestellingen.Site/Controllers/BestellingController.cs
@@ -0,0 +1,26 @@
+﻿using Case3.FEBestellingen.Site.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Case3.FEBestellingen.Site.Controllers
+{
+    public class BestellingController : Controller
+    {
+        // GET: Bestelling
+        public ActionResult Index()
+        {
+            //dummy data, delete this when real data is available
+            var artikel1 = new ArtikelViewModel { Naam = "fietsbel", Leveranciersnaam = "Gazelle", Leverancierscode = "012489", Aantal = 2 };
+            var artikel2 = new ArtikelViewModel { Naam = "zadelpen", Leveranciersnaam = "Gazelle", Leverancierscode = "424521", Aantal = 1 };
+            List<ArtikelViewModel> artikelen = new List<ArtikelViewModel>() { artikel1, artikel2 };
+            var model = new BestellingViewModel { Artikelen = artikelen };
+
+            return View(model);
+        }
+
+
+    }
+}
---
+++ 02-FEBestellingen/Case3.FEBestellingen.Site/ViewModels/ArtikelViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Case3.FEBestellingen.Site.ViewModels
+{
+    public class ArtikelViewModel
+    {
+        public string Naam { get; set; }
+        public string Leveranciersnaam { get; set; }
+        public string Leverancierscode { get; set; }
+        public int Aantal { get; set; }
+    }
+}
