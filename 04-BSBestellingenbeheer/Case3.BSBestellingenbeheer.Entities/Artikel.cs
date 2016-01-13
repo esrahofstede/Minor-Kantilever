efa791af16cde60fd6c9837222843f8e9ad0fbbc
unknown <BartvH@CURSISTZ04.minor.infosupport.edu>
2016-01-13 11:38:47 +0100

    Added entiteiten bestelling & artikel

---
+++ 04-BSBestellingenbeheer/Case3.BSBestellingenbeheer.Entities/Artikel.cs
@@ -0,0 +1,35 @@
+﻿namespace Case3.BSBestellingenbeheer.Entities
+{
+    public class Artikel
+    {
+        private string _naam;
+        private string _leverancierscode;
+        private string _leverancier;
+        private int _aantal;
+
+        public string Naam
+        {
+            get { return _naam; }
+            set { _naam = value; }
+        }
+
+        public int Aantal
+        {
+            get { return _aantal; }
+            set { _aantal = value; }
+        }
+
+        public string Leverancierscode
+        {
+            get { return _leverancierscode; }
+            set { _leverancierscode = value; }
+        }
+
+        public string Leverancier
+        {
+            get { return _leverancier; }
+            set { _leverancier = value; }
+        }
+
+    }
+}
