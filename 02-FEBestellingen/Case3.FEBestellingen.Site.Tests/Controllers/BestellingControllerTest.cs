8a684a4ce058956c3d74a1acb11f39b882bffb44
Boaz Cornellissen <BoazC@minor.infosupport.edu>
2016-01-13 09:41:26 +0100

    Added  comments, tests and resolved all code analysis warnings

---
+++ 02-FEBestellingen/Case3.FEBestellingen.Site.Tests/Controllers/BestellingControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Case3.FEBestellingen.Site.Controllers;
+using System.Web.Mvc;
+using Case3.FEBestellingen.Site.ViewModels;
+
+namespace Case3.FEBestellingen.Site.Tests.Controllers
+{
+    /// <summary>
+    /// Responsible for testing the BestellingController Class
+    /// </summary>
+    [TestClass]
+    public class BestellingControllerTest
+    {
+        [TestMethod]
+        public void BestellingControllerIndexActionReturnsViewResult()
+        {
+            // Arrange
+            var controller = new BestellingController();
+
+            // Act
+            ActionResult result = controller.Index();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+        [TestMethod]
+        public void BestellingControllerIndexActionHasCorrectModel()
+        {
+            // Arrange
+            var controller = new BestellingController();
+
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsInstanceOfType(result.Model, typeof(BestellingViewModel));
+        }
+    }
+}
--- 02-FEBestellingen/Case3.FEBestellingen.Site/Controllers/BestellingController.cs
+++ 02-FEBestellingen/Case3.FEBestellingen.Site/Controllers/BestellingController.cs
@@ -7,9 +7,15 @@ using System.Web.Mvc;
 
 namespace Case3.FEBestellingen.Site.Controllers
 {
+    /// <summary>
+    /// This class is responsible for all interactions with Bestellingen
+    /// </summary>
     public class BestellingController : Controller
     {
-        // GET: Bestelling
+        /// <summary>
+        /// This function returns an overview page of a Bestelling
+        /// </summary>
+        /// <returns>View with products of the Bestelling</returns>
         public ActionResult Index()
         {
             //dummy data, delete this when real data is available
