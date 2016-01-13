07c1bc5c2bd58a9a9bbcccb9c7e2e6741fddf9a4
unknown <BartvH@CURSISTZ04.minor.infosupport.edu>
2016-01-13 10:16:33 +0100

    quick fix to cookies so FE builds correct

--- 01-FEWebwinkel/Case3.FEWebwinkel.Site/Controllers/CatalogusController.cs
+++ 01-FEWebwinkel/Case3.FEWebwinkel.Site/Controllers/CatalogusController.cs
@@ -2,6 +2,7 @@
 using Case3.FEWebwinkel.Site.Managers.Interfaces;
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Case3.FEWebwinkel.Site.Controllers
@@ -12,13 +13,14 @@ namespace Case3.FEWebwinkel.Site.Controllers
     public class CatalogusController : Controller
     {
         private ICatalogusManager _catalogusManager;
+        private HttpCookieCollection _cookies;
 
         /// <summary>
         /// This constructor is the default constructor
         /// </summary>
         public CatalogusController()
         {
-          
+            _cookies = new HttpCookieCollection();
             _catalogusManager = new CatalogusManager();
         }
         /// <summary>
@@ -28,6 +30,16 @@ namespace Case3.FEWebwinkel.Site.Controllers
         public CatalogusController(ICatalogusManager manager)
         {
             _catalogusManager = manager;
+        }
+
+        /// <summary>
+        /// This constructor is for testing purposes
+        /// </summary>
+        /// <param name="manager">This should be a mock of ICatalogusManager</param>
+        public CatalogusController(HttpCookieCollection cookies)
+        {
+            _cookies = cookies;
+
         }
         /// <summary>
         /// This function returns the overview page of the catalog
@@ -45,7 +57,6 @@ namespace Case3.FEWebwinkel.Site.Controllers
         /// </summary>
         /// <param name="articleID">The ID of the chosen article</param>
         [HttpPost]
-        //[ExcludeFromCodeCoverage]
         public ActionResult Index(int articleID)
         {
             
@@ -53,14 +64,15 @@ namespace Case3.FEWebwinkel.Site.Controllers
             CookieNator<Guid> cookieNator;
             try //to get the userGuid from an existing cookie
             {
-                cookieNator = new CookieNator<Guid>(Request.Cookies);
+                
+                cookieNator = new CookieNator<Guid>(_cookies);
                 userGuid = cookieNator.GetCookieValue("UserGuid");
             }
             catch (NullReferenceException) //Create a new userGuid if cookie can't be found
             {
                 userGuid = Guid.NewGuid().ToString();
                 //Set cookie for later usage
-                cookieNator = new CookieNator<Guid>(Request.Cookies);
+                cookieNator = new CookieNator<Guid>(_cookies);
                 cookieNator.CreateCookieWithUserGuid(userGuid);
             }
 
