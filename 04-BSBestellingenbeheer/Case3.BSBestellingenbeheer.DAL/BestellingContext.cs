9560eb32b93bb1a205db46f47477c85e3b4adfc3
unknown <BartvH@CURSISTZ04.minor.infosupport.edu>
2016-01-13 11:50:13 +0100

    BestellingContext

---
+++ 04-BSBestellingenbeheer/Case3.BSBestellingenbeheer.DAL/BestellingContext.cs
@@ -0,0 +1,22 @@
+﻿using Case3.BSBestellingenbeheer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Case3.BSBestellingenbeheer.DAL
+{
+    public class BestellingContext : DbContext
+    {
+        public BestellingContext()
+        {
+            this.Configuration.LazyLoadingEnabled = true;
+        }
+
+        public DbSet<Bestelling> Bestellingen { get; set; }
+        public DbSet<Artikel> Artikellen { get; set; }
+    }
+}
+}
