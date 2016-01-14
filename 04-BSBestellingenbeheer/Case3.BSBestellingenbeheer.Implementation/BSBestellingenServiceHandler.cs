b10ec989483b5c664caaa895445f4e745af98025
Dennis Meijer <dnns11@hotmail.com>
2016-01-14 11:40:36 +0100

    Work in progres

--- 04-BSBestellingenbeheer/Case3.BSBestellingenbeheer.Implementation/BSBestellingenServiceHandler.cs
+++ 04-BSBestellingenbeheer/Case3.BSBestellingenbeheer.Implementation/BSBestellingenServiceHandler.cs
@@ -1,14 +1,33 @@
 ﻿using Case3.BSBestellingenbeheer.Contract;
+using Case3.BSBestellingenbeheer.Implementation.Interfaces;
+using Case3.BSBestellingenbeheer.Implementation.Managers;
 using Case3.BSBestellingenbeheer.V1.Messages;
+using Case3.BSBestellingenbeheer.V1.Schema;
+using Case3.BSCatalogusBeheer.Schema.ProductNS;
 using System;
 
 namespace Case3.BSBestellingenbeheer.Implementation
 {
     public class BSBestellingenServiceHandler : IBSBestellingenbeheerService
     {
+        private IBestellingManager _bestellingManager;
+
+        public BSBestellingenServiceHandler()
+        {
+            _bestellingManager = new BestellingManager();
+        }
+
+        public BSBestellingenServiceHandler(IBestellingManager bestellingManager)
+        {
+            _bestellingManager = bestellingManager;
+        }
+
         public FindFirstBestellingResultMessage FindFirstBestelling(FindFirstBestellingRequestMessage requestMessage)
         {
-            throw new NotImplementedException();
+            return new FindFirstBestellingResultMessage()
+            {
+                BestellingOpdracht = _bestellingManager.FindFirstBestelling()
+            };
         }
     }
 }
---
+++ 04-BSBestellingenbeheer/Case3.BSBestellingenbeheer.Implementation/Managers/BestellingManager.cs
@@ -0,0 +1,52 @@
+﻿using Case3.BSBestellingenbeheer.Implementation.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Case3.BSBestellingenbeheer.V1.Schema;
+using Case3.BSBestellingenbeheer.DAL;
+using Case3.BSCatalogusBeheer.Schema.ProductNS;
+
+namespace Case3.BSBestellingenbeheer.Implementation.Managers
+{
+    public class BestellingManager : IBestellingManager
+    {
+        public Bestelling FindFirstBestelling()
+        {
+
+            Bestelling bestelling = new Bestelling();
+
+            BestellingDataMapper bestellingDataMappper = new BestellingDataMapper();
+
+            Entities.Bestelling bestellingEntity = null;
+
+            using (var context = new BestellingContext())
+            {
+                bestellingEntity = bestellingDataMappper.GetBestellingToPack(context);
+            }
+            
+            if (bestellingEntity != null)
+            {
+                if(bestellingEntity.Artikelen.Count > 0)
+                {
+                    foreach (Entities.Artikel artikel in bestellingEntity.Artikelen)
+                    {
+                        bestelling.Artikelen.Add(new BestelItem()
+                        {
+                            Product = new Product ()
+                            {
+                                Naam = artikel.Naam,
+                                LeverancierNaam = artikel.Leverancier,
+                                LeveranciersProductId = artikel.Leverancierscode,
+                            },
+                            Aantal = artikel.Aantal
+                        });
+                    }
+                }
+            }
+
+            return bestelling;
+        }
+    }
+}
