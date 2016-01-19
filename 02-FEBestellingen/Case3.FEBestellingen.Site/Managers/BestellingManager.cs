42254237431fa657bf7e100bee943480fce1af42
Rogier Welten <rogierwelten@gmail.com>
2016-01-19 15:52:10 +0100

    Bestellingmanager mapt de invoer van Agent om naar viewmodel

--- 02-FEBestellingen/Case3.FEBestellingen.Site/Managers/BestellingManager.cs
+++ 02-FEBestellingen/Case3.FEBestellingen.Site/Managers/BestellingManager.cs
@@ -5,6 +5,8 @@ using Case3.FEBestellingen.Site.Managers.Interfaces;
 using Case3.FEBestellingen.Site.ViewModels;
 using Case3.PcSBestellen.V1.Messages;
 using case3pcsbestellen.v1.schema;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Case3.FEBestellingen.Site.Managers
@@ -54,41 +56,53 @@ namespace Case3.FEBestellingen.Site.Managers
         /// <returns>Returns a BestellingViewModel<returns>
         public BestellingViewModel ConvertBestellingToBestellingViewModel(BestellingPcS bestelling)
         {
-            BestellingViewModel bestellingModel = new BestellingViewModel();
+            BestellingViewModel bestellingModel = null;
 
             //Create Artikelen based on the Bestelling
             if (bestelling != null)
             {
+                List<ArtikelViewModel> artikelen = GetArtikelListFromBestelling(bestelling);
 
-                var artikelen = bestelling.ArtikelenPcS.Select(art => new ArtikelViewModel
+                bestellingModel = new BestellingViewModel
                 {
-                    ArtikelNaam = art.Product.Naam,
-                    Prijs = art.Product.Prijs,
-                    Leveranciersnaam = art.Product.LeverancierNaam,
-                    Leverancierscode = art.Product.LeveranciersProductId,
-                    Aantal = art.Aantal,
-                }).ToList();
-
-                //Add the Artikelen to the BestellingViewModel
-                bestellingModel.Artikelen = artikelen;
-
-                //bestellingModel.FactuurNummer = (int)bestelling.FactuurID;
-                
+                    Artikelen = artikelen,
+                    Adresregel1 = bestelling.Klantgegevens.Adresregel1,
+                    Adresregel2 = bestelling.Klantgegevens.Adresregel2,
+                    //FactuurDatum = bestelling.FactuurDatum,
+                    FactuurDatum = DateTime.Now,
+                    FactuurNummer = 10000 + bestelling.BestellingID,
+                    KlantNaam = bestelling.Klantgegevens.Naam,
+                    Postcode = bestelling.Klantgegevens.Postcode,
+                    Woonplaats = bestelling.Klantgegevens.Woonplaats,
+                };
 
                 //Calculate total prices and BTW
-                bestellingModel = CalculateTotalPricesBestellingViewModel(bestellingModel);
-            
+                CalculateTotalPricesBestellingViewModel(ref bestellingModel);
             }
-
             return bestellingModel;
+        }
+        /// <summary>
+        /// This function creates a list with ArtikelViewModels of the given BestellingPcS
+        /// </summary>
+        /// <param name="bestelling">The BestellingPcS of which the Artikelen has to be made</param>
+        /// <returns>Returns a List of ArtikelViewModels of the given BestellingPcS</returns>
+        private List<ArtikelViewModel> GetArtikelListFromBestelling(BestellingPcS bestelling)
+        {
+            return bestelling.ArtikelenPcS.Select(art => new ArtikelViewModel
+            {
+                ArtikelNaam = art.Product.Naam,
+                Prijs = art.Product.Prijs,
+                Leveranciersnaam = art.Product.LeverancierNaam,
+                Leverancierscode = art.Product.LeveranciersProductId,
+                Aantal = art.Aantal,
+            }).ToList();
         }
 
         /// <summary>
         /// Computes the btw and total price for the bestellingviewmodel
         /// </summary>
         /// <param name="bestellingViewModel">The bestellingviewmodel which needs to be processed</param>
-        /// <returns>Bestellingviewmodel with computed values</returns>
-        public BestellingViewModel CalculateTotalPricesBestellingViewModel(BestellingViewModel bestellingViewModel)
+        private void CalculateTotalPricesBestellingViewModel(ref BestellingViewModel bestellingViewModel)
         {
             if(bestellingViewModel != null)
             {
@@ -106,8 +120,6 @@ namespace Case3.FEBestellingen.Site.Managers
                 // Calculate the BTW for the price
                 bestellingViewModel.TotaalBTW = _btwCalculator.CalculateBTWOfPrice(totaalExclBTW);
             }
-
-            return bestellingViewModel;
         }
     }
 }
