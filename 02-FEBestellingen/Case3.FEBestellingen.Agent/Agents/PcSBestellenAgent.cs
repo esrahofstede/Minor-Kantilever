c07681f61386236a4f397f9d316e84cec78ae8b8
Rogier Welten <rogierwelten@gmail.com>
2016-01-19 15:51:47 +0100

    Agent geeft nu voorgedefinieerde lijst terug

--- 02-FEBestellingen/Case3.FEBestellingen.Agent/Agents/PcSBestellenAgent.cs
+++ 02-FEBestellingen/Case3.FEBestellingen.Agent/Agents/PcSBestellenAgent.cs
@@ -3,6 +3,7 @@ using Case3.FEBestellingen.Agent.Interfaces;
 using Case3.PcSBestellen.V1.Messages;
 using Minor.ServiceBus.Agent.Implementation;
 using case3pcsbestellen.v1.schema;
+using Case3.BSCatalogusBeheer.Schema.ProductNSPcS;
 
 namespace Case3.FEBestellingen.Agent.Agents
 {
@@ -13,8 +14,8 @@ namespace Case3.FEBestellingen.Agent.Agents
 
         public PcSBestellenAgent()
         {
-            _factory = new ServiceFactory<IPcSBestellenService>("PcSBestellen");
-            _agent = _factory.CreateAgent();
+            //_factory = new ServiceFactory<IPcSBestellenService>("PcSBestellen");
+            //_agent = _factory.CreateAgent();
         }
 
         public PcSBestellenAgent(IPcSBestellenService agent)
@@ -23,8 +24,50 @@ namespace Case3.FEBestellingen.Agent.Agents
         }
         public BestellingPcS FindNextBestelling(FindNextBestellingRequestMessage requestMessage)
         {
-            FindNextBestellingResultMessage result = _agent.FindNextBestelling(requestMessage);
-            return result.BestellingOpdracht;
+            //FindNextBestellingResultMessage result = _agent.FindNextBestelling(requestMessage);
+            //return result.BestellingOpdracht;
+            return new BestellingPcS
+            {
+                BestellingID = 1,
+                FactuurID = 10001,
+                Status = "1",
+                Klantgegevens = new KlantgegevensPcS
+                {
+                    Naam = "Rogier Welten",
+                    Adresregel1 = "Haagstraat 5",
+                    Adresregel2 = "t.a.v. Dhr. Welten",
+                    Postcode = "4921XA",
+                    Telefoonnummer = "0627232007",
+                    Woonplaats = "Made",
+                },
+                ArtikelenPcS = new ArtikelenPcS
+                {
+                    new BestelItemPcS
+                    {
+                        Aantal = 1,
+                        Product = new Product
+                        {
+                            Id = 1,
+                            Naam = "Fietsbel",
+                            LeverancierNaam = "Gazelle",
+                            LeveranciersProductId = "GA123456FB",
+                            Prijs = 4.95M,
+                        }
+                    },
+                    new BestelItemPcS
+                    {
+                        Aantal = 2,
+                        Product = new Product
+                        {
+                            Id = 2,
+                            Naam = "Zadelpen",
+                            LeverancierNaam = "Giant",
+                            LeveranciersProductId = "GI123456ZP",
+                            Prijs = 4.95M,
+                        }
+                    },
+                },
+            };
         }
     }
 }
